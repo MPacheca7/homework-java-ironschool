84c79ca41eaf34916fef30adca4160fcd3741369
Javier Moneo <jvr.moneo@gmail.com>
2025-04-14 18:18:22 +0200

    Create interface IRegistration, class Adminsitration and override methods in Registration

---
+++ IronSchool/src/main/java/IRegistration.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public interface IRegistration {
+    void enroll(String studentId, String courseId);
+
+    void assing(String teacherId, String courseId);
+
+    void showCourses();
+
+    void lookupCourse(String courseId);
+
+    void showStudents();
+
+    void lookupStudent(String studentId);
+
+    void showTeachers();
+
+    void lookupTeacher(String teacherId);
+
+    double showProfit();
+}
