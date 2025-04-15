ee00d06951ca1c72e8281f968053953857b8be1a
Pacheca <mpacheca7@gmail.com>
2025-04-15 11:55:09 +0200

    Teacher init

--- IronSchool/src/main/java/Teacher.java
+++ IronSchool/src/main/java/Teacher.java
@@ -1,2 +1,37 @@
+import java.util.UUID;
+
 public class Teacher {
+    private String teacherId;
+    private String name;
+    private double salary;
+
+    public Teacher(double salary, String name) {
+        this.teacherId = UUID.randomUUID().toString();
+        this.salary = salary;
+        this.name = name;
+    }
+
+    public Teacher(String teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getTeacherId() {
+        return teacherId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
